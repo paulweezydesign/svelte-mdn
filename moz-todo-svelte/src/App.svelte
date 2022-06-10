47d2ec3f7fea87df351f0d03ba5632c7151c2c95
paulweezydesign <weezy@paulweezydesign.com>
2022-06-10 04:02:32 -0700

    Initial commit

---
+++ moz-todo-svelte/src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	import NavBar from  './src/lic/NavBar.svelte';
+  import  './public/app.css'
+</script>
+<NavBar />
+<main>
+  <h1 class="text-9xl text-center gradient">Tailwind + Svelte</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+
+</style>
